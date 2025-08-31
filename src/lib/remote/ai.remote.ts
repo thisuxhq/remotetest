a0847d5c528bd129b9ca2e6d4ac4e0279ebace73
SpikeySanju <spikeysanju98@gmail.com>
2025-08-31 15:47:49 +0530

    add origin test

--- src/lib/remote/ai.remote.ts
+++ src/lib/remote/ai.remote.ts
@@ -1,6 +1,10 @@
-import { command } from '$app/server';
+import { command, getRequestEvent } from '$app/server';
 
 export const simulateAIGeneration = command('unchecked', async (payload) => {
+	const { request } = getRequestEvent();
+	const origin = request.headers.get('origin');
+	console.log('origin :', origin);
+
 	return {
 		enhancedText: 'This is a simulated AI generation: ' + payload.text
 	};
